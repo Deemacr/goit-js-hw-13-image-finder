b3b9551d2a8221eec1dda638f1ded471ba40c849
Viktor Kirichok <viktor.kirichok@gmail.com>
2020-08-09 13:05:06 +0300

    HW 13.1. added

--- src/js/script.js
+++ src/js/script.js
@@ -5,28 +5,26 @@ import tmp from './templating.js'
 let searchQuery = "";
 let page = 1;
 
-// РАБОТАЕТ !ТОЛЬКО! НА НАЖАТИЕ Enter
 
-refs.searchForm.addEventListener('keypress', (event) => {
-	if (event.key === "Enter") {
-		event.preventDefault();
-		page = 1;
-		refs.loadMore.style.visibility = "visible";
-		searchQuery = event.currentTarget.elements['query'].value;
-		if (!searchQuery) {
-			refs.loadMore.style.visibility = "hidden";
-			refs.galleryWrap.innerHTML = "";
-			return;
-		}
-		return apiService.fetchPics(searchQuery, page).then(pics => { tmp.updateGallery(pics); page += 1 });
+refs.searchForm.addEventListener('submit', (event) => {
+	event.preventDefault();
+	page = 1;
+	refs.loadMore.style.visibility = "visible";
+	searchQuery = event.currentTarget.elements['query'].value;
+	if (!searchQuery) {
+		refs.loadMore.style.visibility = "hidden";
+		refs.galleryWrap.innerHTML = "";
+		return;
 	}
+	return apiService.fetchPics(searchQuery, page).then(pics => { if (pics.length < 12) { refs.loadMore.style.visibility = "hidden" }; tmp.updateGallery(pics); page += 1 });
 });
 
-// return apiService.fetchPics(searchQuery, page).then(tmp.updateGallery);
 
 refs.loadMore.addEventListener('click', (event) => {
 	apiService.fetchPics(searchQuery, page).then(tmp.loadMorePics).then(page += 1);
-	window.scrollTo(0, document.body.scrollHeight, { behaviour: 'smooth' });
+	window.scrollTo(0, document.body.scrollHeight, {
+		behavior: "smooth"
+	});
 });
 
 
